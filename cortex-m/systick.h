9be326ac54b7ea87ee88ce0de013dd9d276dd5a3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-04-21 20:20:53 +0300

    Add Cortex-M SysTick definitions.
    Based on Energy Micro docs.

---
+++ cortex-m/systick.h
@@ -0,0 +1,50 @@
+/*
+ * ARM Cortex-M SysTick Register Definitions
+ *
+ * This file is a part of libperipha library:
+ * https://github.com/pfalcon/libperipha
+ *
+ * Copyright (c) 2013 Paul Sokolovsky
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Paul Sokolovsky nor the names of other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define __rw volatile
+#define __ro volatile const
+
+struct SysTick_ {
+    __rw uint32_t CTRL;
+    __rw uint32_t LOAD;
+    __rw uint32_t VAL;
+    __ro uint32_t CALIB;
+};
+
+#define SysTick_CTRL_ENABLE     0x0001
+#define SysTick_CTRL_TICKINT    0x0002
+#define SysTick_CTRL_COUNTFLAG  0x0100
+
+#define SysTick_Base 0xE000E010
+#define SysTick ((struct SysTick_*)SysTick_Base)
