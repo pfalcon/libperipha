f9c32340605a8e79be4f616592135023cb300be8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-05-25 23:31:03 +0300

    Double-include protection.
    Give up on include guards, #pragma once appears to be supported on
    all relevant compilers: http://en.wikipedia.org/wiki/Pragma_once .

--- arm/cortex-m/systick.h
+++ arm/cortex-m/systick.h
@@ -31,6 +31,8 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#pragma once
+
 #include <libperipha_types.h>
 
 struct SysTick_ {
